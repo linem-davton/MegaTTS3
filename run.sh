61b9920b1f28eeb8e2f6ef555c715d805dc38761
linem davton <linem.davton@gmail.com>
2025-06-20 10:05:41 +0200

    Voices: female narration

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 
 source venv/bin/activate  # Activate the virtual environment
 # Set the root directory
-export PYTHONPATH="/path/to/MegaTTS3:$PYTHONPATH"
+export PYTHONPATH="./tts:$PYTHONPATH"
 
 # [Optional] Set GPU
 export CUDA_VISIBLE_DEVICES=0
 
-python3 tts/infer_cli.py --input_wav 'assets/English_prompt.wav' --input_text 'Some Text' --output_dir ./gen --p_w 2.0 --t_w 3.0
+python3 tts/infer_cli.py --input_wav 'assets/narrationfemale.wav' --input_text 'Some Text' --output_dir ./gen --p_w 2.0 --t_w 3.0
